379f1723f05c2919832582d54e538ac7e9faab4e
shotasan <wondermoai@gmail.com>
2019-02-04 17:05:36 +0900

    ログイン画面にエラーメッセージを実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    # sessions/new/html.erbで入力されたemailが正しいかどうか
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+    else
+      flash.now[:danger] = "ログインに失敗しました"
+      render "new"
+    end
+  end
 end
