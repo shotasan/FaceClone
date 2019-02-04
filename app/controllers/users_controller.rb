e5644e08a02ca74335c27a4bf9fdf4f9f95d4929
shotasan <wondermoai@gmail.com>
2019-02-04 16:48:40 +0900

    ログイン画面にエラーメッセージを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    # new.html.erbから受け取ったパラメーターでインスタンスを作成
+    @user = User.new(user_params)
+    if @user.save
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def user_params
+    # new.html.erbからパラメーターを受ける
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
