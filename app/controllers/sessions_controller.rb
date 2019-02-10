3b1c1cb4af057fa16347815fa857257e2dfb2591
shotasan <wondermoai@gmail.com>
2019-02-11 03:29:44 +0900

    投稿一覧画面のデザインを修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,15 +12,15 @@ class SessionsController < ApplicationController
       redirect_to user_path(user.id)
     else
       
-      flash.now[:danger] = "ログインに失敗しました"   
-      render "new"
+      flash[:danger] = "ログインに失敗しました"   
+      redirect_to "/"
     end
   end
 
   def destroy
     session.delete(:user_id)
     flash[:notice] = "ログアウトしました"
-    redirect_to new_session_path
+    redirect_to "/"
   end
 
 
