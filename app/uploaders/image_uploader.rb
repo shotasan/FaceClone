c142aab2ed14c70f3ad3bf7752be51c64bcad76c
shotasan <wondermoai@gmail.com>
2019-02-13 19:30:21 +0900

    サイドバーを修正。投稿一覧画面を修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -45,5 +45,5 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   "something.jpg" if original_filename
   # end
 
-  process resize_to_limit: [100,100]
+  process resize_to_limit: [300,300]
 end
