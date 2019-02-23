8f0e62e38617c9d2e3366a8e91282464a49947ee
shotasan <wondermoai@gmail.com>
2019-02-23 13:34:49 +0900

    issuesを２つ完了

---
+++ app/models/abc.rb
@@ -0,0 +1,5 @@
+module Abc
+  def self.table_name_prefix
+    'abc_'
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,9 @@ module FacebookClone
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Tokyo'
+    config.generators do |g|
+      g.assets     false
+      g.helper     false
+    end
   end
 end
